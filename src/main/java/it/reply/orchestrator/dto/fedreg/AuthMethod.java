7501e52174b8ac539545d9dd72ae44da4a0ac2fd
Luca Giommi <luca.giommi@cnaf.infn.it>
2025-10-01 11:42:13 +0200

    make optional idp_name and protocol for AuthMethod

--- src/main/java/it/reply/orchestrator/dto/fedreg/AuthMethod.java
+++ src/main/java/it/reply/orchestrator/dto/fedreg/AuthMethod.java
@@ -18,7 +18,6 @@
 package it.reply.orchestrator.dto.fedreg;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,11 +31,9 @@ import lombok.NoArgsConstructor;
 public class AuthMethod {
 
   @JsonProperty("idp_name")
-  @NotNull
   private String idpName;
 
   @JsonProperty("protocol")
-  @NotNull
   private String protocol;
 
   @JsonProperty("audience")
